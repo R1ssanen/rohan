44ea075ce377a622fb98ce65eb679538c9b99d4b
Aaro Rissanen <aaro.rissanen16@gmail.com>
2026-02-01 10:50:55 +0200

    refactor: removed rendering options

--- rohan.c
+++ rohan.c
@@ -92,40 +92,42 @@ void rohan_init(void)
     multiplier_masks[7] = _mm256_setr_ps(-7.f, -6.f, -5.f, -4.f, -3.f, -2.f, -1.f, 0.f);
 }
 
-static inline void advance(rohan_raster_state *restrict state, const rohan_shader_spec *restrict spec,
+static inline void advance(rohan_raster_state *restrict state, const rohan_shader_desc *restrict desc,
                            size_t target_stride, void *restrict instance, __m256 *restrict attributes,
                            const __m256 *restrict dattr_dx)
 {
-    spec->main(instance, state);
-    state->pos.x = _mm256_add_ps(state->pos.x, _mm256_set1_ps(8));
+    desc->main(instance, state);
+    state->pos.x = _mm256_add_ps(state->pos.x, _mm256_set1_ps(8.f));
     state->byte_offset += 8 * target_stride;
 
-    for (size_t i = 0; i < spec->attribute_count; ++i)
+    for (size_t i = 0; i < desc->attribute_count; ++i)
     {
         attributes[i] = _mm256_add_ps(attributes[i], dattr_dx[i]);
     }
 }
 
-static void rasterize(const rohan_shader_spec *restrict spec, void *restrict instance, size_t target_pitch,
-                      size_t target_stride, uint32_t primitive_id, const float *restrict attr_0,
-                      const __m256 *restrict dattr_dx, const __m256 *restrict dattr_dy, float x_left, float dx_dy_left,
-                      float x_right, float dx_dy_right, int y_top, int y_bottom)
+static void rasterize(const rohan_shader_desc *restrict desc, void *restrict instance, size_t target_pitch,
+                      size_t target_stride, const float *restrict attr_0, const __m256 *restrict dattr_dx,
+                      const __m256 *restrict dattr_dy, float x_left, float dx_dy_left, float x_right, float dx_dy_right,
+                      int y_top, int y_bottom)
 {
-    rohan_raster_state state = {.primitive_id = primitive_id};
-    __m256 seq = _mm256_setr_ps(0, 1, 2, 3, 4, 5, 6, 7);
+    rohan_raster_state state = {.pos = {.y = _mm256_set1_ps(y_top)}};
 
-    __m256 *attributes = (__m256 *)(((char *)instance) + spec->attribute_offset);
+    __m256 *attributes = (__m256 *)((uint8_t *)instance + desc->attribute_offset);
+    __m256 seq = _mm256_setr_ps(0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f);
+    __m256 one = _mm256_set1_ps(1.f);
 
     __m256 dattr_dx_step[ROHAN_MAX_ATTRIBUTES];
     __m256 attr_00[ROHAN_MAX_ATTRIBUTES];
-    for (size_t i = 0; i < spec->attribute_count; ++i)
+
+    size_t row_byte_offset = y_top * target_pitch;
+    for (size_t i = 0; i < desc->attribute_count; ++i)
     {
-        dattr_dx_step[i] = _mm256_mul_ps(dattr_dx[i], _mm256_set1_ps(8));
+        dattr_dx_step[i] = _mm256_mul_ps(dattr_dx[i], _mm256_set1_ps(8.f));
         attr_00[i] = _mm256_set1_ps(attr_0[i]);
     }
 
-    size_t byte_offset = y_top * target_pitch;
-    for (int y = y_top; y < y_bottom; ++y, byte_offset += target_pitch)
+    for (int y = y_top; y < y_bottom; ++y, row_byte_offset += target_pitch)
     {
         int x0 = ceilf(x_left);
         int x1 = floorf(x_right);
@@ -138,53 +140,54 @@ static void rasterize(const rohan_shader_spec *restrict spec, void *restrict ins
         int x1_aligned = x1 & ~7;
         int header = x0 - x0_aligned;
 
-        state.byte_offset = byte_offset + x0_aligned * target_stride;
+        state.byte_offset = row_byte_offset + x0_aligned * target_stride;
         state.pos.x = _mm256_add_ps(seq, _mm256_set1_ps(x0_aligned));
-        state.pos.y = _mm256_set1_ps(y);
+        state.pos.y = _mm256_add_ps(state.pos.y, one);
 
         __m256 multiplier_mask = multiplier_masks[header];
-        for (size_t i = 0; i < spec->attribute_count; ++i)
+        for (size_t i = 0; i < desc->attribute_count; ++i)
         {
             attributes[i] = _mm256_fmadd_ps(multiplier_mask, dattr_dx[i], attr_00[i]);
         }
 
-        if (x0_aligned == x1_aligned)
+        int aligned_diff = x1_aligned - x0_aligned;
+        if (aligned_diff == 0)
         {
             state.mask = infix_masks[header][x1 - x0_aligned];
-            spec->main(instance, &state);
+            desc->main(instance, &state);
         }
         else
         {
             state.mask = prefix_masks[header];
-            advance(&state, spec, target_stride, instance, attributes, dattr_dx_step);
+            advance(&state, desc, target_stride, instance, attributes, dattr_dx_step);
             state.mask = _mm256_set1_epi32(-1);
 
-            int steps = (x1_aligned - x0_aligned) >> 3;
+            int steps = aligned_diff >> 3;
             for (int i = 1; i < steps; ++i)
             {
-                advance(&state, spec, target_stride, instance, attributes, dattr_dx_step);
+                advance(&state, desc, target_stride, instance, attributes, dattr_dx_step);
             }
 
             state.mask = postfix_masks[x1 - x1_aligned];
-            spec->main(instance, &state);
+            desc->main(instance, &state);
         }
 
     interpolate:
         x_left += dx_dy_left;
         x_right += dx_dy_right;
 
-        for (size_t i = 0; i < spec->attribute_count; ++i)
+        for (size_t i = 0; i < desc->attribute_count; ++i)
         {
             attr_00[i] = _mm256_add_ps(attr_00[i], dattr_dy[i]);
         }
     }
 }
 
-static void render_triangle(const rohan_shader_spec *restrict spec, void *restrict instance, size_t target_pitch,
-                            size_t target_stride, uint32_t primitive_id, float x0, float y0, float x1, float y1,
-                            float x2, float y2, const float *restrict attr_0, const float *restrict attr_1,
-                            const float *restrict attr_2)
+void rohan_rasterize(const rohan_shader_desc *restrict desc, void *restrict instance, size_t target_pitch,
+                     size_t target_stride, float x0, float y0, float x1, float y1, float x2, float y2,
+                     const float *restrict attr_0, const float *restrict attr_1, const float *restrict attr_2)
 {
+
     y0 = floorf(y0);
     y1 = floorf(y1);
     y2 = floorf(y2);
@@ -220,26 +223,26 @@ static void render_triangle(const rohan_shader_spec *restrict spec, void *restri
 
         if (x0 < x1)
         {
-            for (size_t i = 0; i < spec->attribute_count; ++i)
+            for (size_t i = 0; i < desc->attribute_count; ++i)
             {
                 dattr_dx[i] = _mm256_set1_ps((attr_1[i] - attr_0[i]) * inv_dx_ab);
                 dattr_dy[i] = _mm256_set1_ps((attr_2[i] - attr_0[i]) * inv_dy_ac);
             }
 
-            rasterize(spec, instance, target_pitch, target_stride, primitive_id, attr_0, dattr_dx, dattr_dy, x0,
-                      dxdy_left, x1, dxdy_right, y0, y2);
+            rasterize(desc, instance, target_pitch, target_stride, attr_0, dattr_dx, dattr_dy, x0, dxdy_left, x1,
+                      dxdy_right, y0, y2);
         }
         else
         {
             float inv_dy_bc = 1.f / (y2 - y1);
-            for (size_t i = 0; i < spec->attribute_count; ++i)
+            for (size_t i = 0; i < desc->attribute_count; ++i)
             {
                 dattr_dx[i] = _mm256_set1_ps((attr_0[i] - attr_1[i]) * (-inv_dx_ab));
                 dattr_dy[i] = _mm256_set1_ps((attr_2[i] - attr_1[i]) * inv_dy_bc);
             }
 
-            rasterize(spec, instance, target_pitch, target_stride, primitive_id, attr_1, dattr_dx, dattr_dy, x1,
-                      dxdy_right, x0, dxdy_left, y0, y2);
+            rasterize(desc, instance, target_pitch, target_stride, attr_1, dattr_dx, dattr_dy, x1, dxdy_right, x0,
+                      dxdy_left, y0, y2);
         }
     }
 
@@ -252,25 +255,25 @@ static void render_triangle(const rohan_shader_spec *restrict spec, void *restri
         if (x1 < x2)
         {
             float inv_dy_ab = 1.f / (y1 - y0);
-            for (size_t i = 0; i < spec->attribute_count; ++i)
+            for (size_t i = 0; i < desc->attribute_count; ++i)
             {
                 dattr_dx[i] = _mm256_set1_ps((attr_2[i] - attr_1[i]) * inv_dx_bc);
                 dattr_dy[i] = _mm256_set1_ps((attr_1[i] - attr_0[i]) * inv_dy_ab);
             }
 
-            rasterize(spec, instance, target_pitch, target_stride, primitive_id, attr_0, dattr_dx, dattr_dy, x0,
-                      dxdy_left, x0, dxdy_right, y0, y2);
+            rasterize(desc, instance, target_pitch, target_stride, attr_0, dattr_dx, dattr_dy, x0, dxdy_left, x0,
+                      dxdy_right, y0, y2);
         }
         else
         {
-            for (size_t i = 0; i < spec->attribute_count; ++i)
+            for (size_t i = 0; i < desc->attribute_count; ++i)
             {
                 dattr_dx[i] = _mm256_set1_ps((attr_1[i] - attr_2[i]) * (-inv_dx_bc));
                 dattr_dy[i] = _mm256_set1_ps((attr_2[i] - attr_0[i]) * inv_dy_ac);
             }
 
-            rasterize(spec, instance, target_pitch, target_stride, primitive_id, attr_0, dattr_dx, dattr_dy, x0,
-                      dxdy_right, x0, dxdy_left, y0, y2);
+            rasterize(desc, instance, target_pitch, target_stride, attr_0, dattr_dx, dattr_dy, x0, dxdy_right, x0,
+                      dxdy_left, y0, y2);
         }
     }
 
@@ -288,136 +291,42 @@ static void render_triangle(const rohan_shader_spec *restrict spec, void *restri
         float inv_dy_bc = 1.f / (y2 - y1);
 
         float attr_3[ROHAN_MAX_ATTRIBUTES];
-        for (size_t i = 0; i < spec->attribute_count; ++i)
+        for (size_t i = 0; i < desc->attribute_count; ++i)
         {
             attr_3[i] = fmaf((attr_2[i] - attr_0[i]) * inv_dy_ac, dy_ab, attr_0[i]);
         }
 
         if (x1 < x3)
         {
-            for (size_t i = 0; i < spec->attribute_count; ++i)
+            for (size_t i = 0; i < desc->attribute_count; ++i)
             {
                 dattr_dx[i] = _mm256_set1_ps((attr_3[i] - attr_1[i]) * inv_dx_bd);
                 dattr_dy[i] = _mm256_set1_ps((attr_1[i] - attr_0[i]) * inv_dy_ab);
             }
 
-            rasterize(spec, instance, target_pitch, target_stride, primitive_id, attr_0, dattr_dx, dattr_dy, x0,
-                      dxdy_top, x0, dxdy_ac, y0, y1);
+            rasterize(desc, instance, target_pitch, target_stride, attr_0, dattr_dx, dattr_dy, x0, dxdy_top, x0,
+                      dxdy_ac, y0, y1);
 
-            for (size_t i = 0; i < spec->attribute_count; ++i)
+            for (size_t i = 0; i < desc->attribute_count; ++i)
             {
                 dattr_dy[i] = _mm256_set1_ps((attr_2[i] - attr_1[i]) * inv_dy_bc);
             }
 
-            rasterize(spec, instance, target_pitch, target_stride, primitive_id, attr_1, dattr_dx, dattr_dy, x1,
-                      dxdy_bottom, x3, dxdy_ac, y1, y2);
+            rasterize(desc, instance, target_pitch, target_stride, attr_1, dattr_dx, dattr_dy, x1, dxdy_bottom, x3,
+                      dxdy_ac, y1, y2);
         }
         else
         {
-            for (size_t i = 0; i < spec->attribute_count; ++i)
+            for (size_t i = 0; i < desc->attribute_count; ++i)
             {
                 dattr_dx[i] = _mm256_set1_ps((attr_1[i] - attr_3[i]) * (-inv_dx_bd));
                 dattr_dy[i] = _mm256_set1_ps((attr_3[i] - attr_0[i]) * inv_dy_ab);
             }
 
-            rasterize(spec, instance, target_pitch, target_stride, primitive_id, attr_0, dattr_dx, dattr_dy, x0,
-                      dxdy_ac, x0, dxdy_top, y0, y1);
-            rasterize(spec, instance, target_pitch, target_stride, primitive_id, attr_3, dattr_dx, dattr_dy, x3,
-                      dxdy_ac, x1, dxdy_bottom, y1, y2);
+            rasterize(desc, instance, target_pitch, target_stride, attr_0, dattr_dx, dattr_dy, x0, dxdy_ac, x0,
+                      dxdy_top, y0, y1);
+            rasterize(desc, instance, target_pitch, target_stride, attr_3, dattr_dx, dattr_dy, x3, dxdy_ac, x1,
+                      dxdy_bottom, y1, y2);
         }
-    }
-}
-
-static void render_line(const rohan_shader_spec *restrict spec, void *restrict instance, size_t target_pitch,
-                        size_t target_stride, int primitive_index, float x0, float y0, float x1, float y1,
-                        const float *restrict attr_0, const float *restrict attr_1)
-{
-    return;
-}
-
-void rohan_render(const rohan_shader_spec *restrict spec, void *restrict instance, size_t target_pitch,
-                  size_t target_stride, const float *restrict vertices, const uint32_t *restrict indices,
-                  size_t index_count, enum rohan_render_mode mode)
-{
-    int attribs = 2 + spec->attribute_count;
-    uint32_t primitive_id = 1;
-    const float *v0, *v1, *v2, *v3;
-
-    switch (mode)
-    {
-    case ROHAN_LINE:
-        for (size_t i = 0; i < index_count; i += 2, ++primitive_id)
-        {
-            v0 = vertices + indices[i] * attribs;
-            v1 = vertices + indices[i + 1] * attribs;
-            render_line(spec, instance, target_pitch, target_stride, primitive_id, v0[0], v0[1], v1[0], v1[1], v0 + 2,
-                        v1 + 2);
-        }
-        break;
-
-    case ROHAN_LINE_STRIP:
-        for (size_t i = 0; i < index_count - 1; i += 1, ++primitive_id)
-        {
-            v0 = vertices + indices[i] * attribs;
-            v1 = vertices + indices[i + 1] * attribs;
-            render_line(spec, instance, target_pitch, target_stride, primitive_id, v0[0], v0[1], v1[0], v1[1], v0 + 2,
-                        v1 + 2);
-        }
-        break;
-
-    case ROHAN_LINE_LOOP:
-        for (size_t i = 0; i < index_count - 1; i += 1, ++primitive_id)
-        {
-            v0 = vertices + indices[i] * attribs;
-            v1 = vertices + indices[i + 1] * attribs;
-            render_line(spec, instance, target_pitch, target_stride, primitive_id, v0[0], v0[1], v1[0], v1[1], v0 + 2,
-                        v1 + 2);
-        }
-
-        primitive_id += 1;
-        v0 = vertices + indices[index_count - 1] * attribs;
-        v1 = vertices + indices[0] * attribs;
-        render_line(spec, instance, target_pitch, target_stride, primitive_id, v0[0], v0[1], v1[0], v1[1], v0 + 2,
-                    v1 + 2);
-        break;
-
-    case ROHAN_TRIANGLE:
-        for (size_t i = 0; i < index_count; i += 3, ++primitive_id)
-        {
-            v0 = vertices + indices[i] * attribs;
-            v1 = vertices + indices[i + 1] * attribs;
-            v2 = vertices + indices[i + 2] * attribs;
-            render_triangle(spec, instance, target_pitch, target_stride, primitive_id, v0[0], v0[1], v1[0], v1[1],
-                            v2[0], v2[1], v0 + 2, v1 + 2, v2 + 2);
-        }
-        break;
-
-    case ROHAN_TRIANGLE_FAN:
-        v0 = vertices + indices[0] * attribs;
-        for (size_t i = 1; i < index_count - 1; i += 1, ++primitive_id)
-        {
-            v1 = vertices + indices[i] * attribs;
-            v2 = vertices + indices[i + 1] * attribs;
-            render_triangle(spec, instance, target_pitch, target_stride, primitive_id, v0[0], v0[1], v1[0], v1[1],
-                            v2[0], v2[1], v0 + 2, v1 + 2, v2 + 2);
-        }
-        break;
-
-    case ROHAN_QUAD:
-        for (size_t i = 0; i < index_count; i += 4, ++primitive_id)
-        {
-            v0 = vertices + indices[i] * attribs;
-            v1 = vertices + indices[i + 1] * attribs;
-            v2 = vertices + indices[i + 2] * attribs;
-            v3 = vertices + indices[i + 3] * attribs;
-            render_triangle(spec, instance, target_pitch, target_stride, primitive_id, v0[0], v0[1], v1[0], v1[1],
-                            v2[0], v2[1], v0 + 2, v1 + 2, v2 + 2);
-            render_triangle(spec, instance, target_pitch, target_stride, primitive_id, v0[0], v0[1], v2[0], v2[1],
-                            v3[0], v3[1], v0 + 2, v2 + 2, v3 + 2);
-        }
-        break;
-
-    default:
-        break;
     }
 }
--- rohan.h
+++ rohan.h
@@ -27,7 +27,7 @@ extern "C"
 #endif
 
 #ifndef ROHAN_MAX_ATTRIBUTES
-#define ROHAN_MAX_ATTRIBUTES 8
+#define ROHAN_MAX_ATTRIBUTES 2
 #endif
 
 #include <immintrin.h>
@@ -43,40 +43,29 @@ extern "C"
         } pos;
 
         __m256i mask;
-        size_t byte_offset; // cluster begin
-        uint32_t primitive_id;
+        size_t byte_offset;
     };
 
-    typedef struct rohan_shader_spec rohan_shader_spec;
-    struct rohan_shader_spec
+    typedef struct rohan_shader_desc rohan_shader_desc;
+    struct rohan_shader_desc
     {
         const char *name;
         void (*main)(void *rohan_restrict instance, const rohan_raster_state *rohan_restrict state);
-        void (*destroy)(rohan_shader_spec *rohan_restrict self);
+        void (*destroy)(rohan_shader_desc *rohan_restrict self);
         size_t attribute_offset;
         size_t attribute_count;
         size_t instance_size;
         size_t uniform_offsets[];
     };
 
-    typedef rohan_shader_spec *(*rohan_get_specification_fn)(void);
-
-    enum rohan_render_mode
-    {
-        ROHAN_LINE,
-        ROHAN_LINE_STRIP,
-        ROHAN_LINE_LOOP,
-        ROHAN_TRIANGLE,
-        ROHAN_TRIANGLE_FAN,
-        ROHAN_QUAD
-    };
+    typedef rohan_shader_desc *(*rohan_get_description_fn)(void);
 
     ROHAN_API void rohan_init(void);
 
-    ROHAN_API void rohan_render(const rohan_shader_spec *rohan_restrict spec, void *rohan_restrict instance,
-                                size_t target_pitch, size_t target_stride, const float *rohan_restrict vertices,
-                                const uint32_t *rohan_restrict indices, size_t index_count,
-                                enum rohan_render_mode mode);
+    ROHAN_API void rohan_rasterize(const rohan_shader_desc *rohan_restrict desc, void *rohan_restrict instance,
+                                   size_t target_pitch, size_t target_stride, float x0, float y0, float x1, float y1,
+                                   float x2, float y2, const float *rohan_restrict attr_0,
+                                   const float *rohan_restrict attr_1, const float *rohan_restrict attr_2);
 
 #ifdef __cplusplus
 }
